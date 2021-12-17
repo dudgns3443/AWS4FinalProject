4e576154f1dbe0dbb53ae22b5becad34497cf835
swlee <allmaster1145@gmail.com>
2021-12-17 10:50:36 +0900

    sg module

--- terraform_configure/05_SG/outputs.tf
+++ terraform_configure/05_SG/outputs.tf
@@ -1,27 +1,27 @@
 output "alb_sg_id" {
-    value = module.alb_sg_id
+    value = module.sg.outputs.alb_sg_id
 }
 
 output "bastion_sg_id" {
-    value = module.bastion_sg_id
+    value = module.sg.outputs.bastion_sg_id
 }
 
 output "db_sg" {
-    value = module.db_sg_id
+    value = module.sg.outputs.db_sg_id
 }
 
 output "efs_sg" {
-    value = module.efs_sg_id
+    value = module.sg.outputs.efs_sg_id
 }
 
 output "gi_sg" {
-    value = module.gi_sg_id
+    value = module.sg.outputs.gi_sg_id
 }
 
 output "was_sg" {
-    value = module.was_sg_id
+    value = module.sg.outputs.was_sg_id
 }
 
 output "web_sg" {
-    value = module.web_sg_id
+    value = module.sg.outputs.web_sg_id
 }
---
+++ terraform_template/sg_module/remote_state_data.tf
@@ -0,0 +1,9 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket = "a4-terraform-state"
+    key    = "network/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
