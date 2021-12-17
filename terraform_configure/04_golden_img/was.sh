47eb414028855f2934cb4d20fec41154b3adb98f
dudgns3443 <williams3443@gmail.com>
2021-12-17 18:04:35 +0900

    modify 07_golden_img

--- terraform_configure/04_golden_img/was.sh
+++ terraform_configure/04_golden_img/was.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 mkdir /app
-cd /app
+
 yum install -y gcc-c++ make 
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
 yum install -y nodejs
 
 yum install git -y
-git clone https://github.com/dudgns3443/HealthCare-BackEnd.git
+git clone https://github.com/dudgns3443/HealthCare-BackEnd.git 
 
-cd HealthCare-BackEnd
+
 npm install package.json --save
-node app
+
+node /app/HealthCare-BackEnd/app &
+#nohup node /app/HealthCare-BackEnd/app &
--- terraform_template/golden_img/07_golden_img.tf
+++ terraform_template/golden_img/07_golden_img.tf
@@ -1,6 +1,20 @@
+#Instance ami Data
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+      name = "name"
+      values = [var.instance_filter_namevalue]
+  }
+  filter {
+      name = "virtualization-type"
+      values = [var.instance_filter_vtypevalue]
+  }
+  owners = [var.instance_owner]
+}
+
 #Web-Golden Image
 resource "aws_instance" "a4_web_golden_image" {
-  ami = "ami-0263588f2531a56bd"
+  ami = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name = var.key
   #vpc_security_group_ids = [aws_security_group.a4_gi_sg.id]
@@ -16,7 +30,7 @@ resource "aws_instance" "a4_web_golden_image" {
 
 #Was-Golden Image
 resource "aws_instance" "a4_was_golden_image" {
-  ami = "ami-0263588f2531a56bd"
+  ami = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name = var.key
   #vpc_security_group_ids = [aws_security_group.a4_gi_sg.id]
