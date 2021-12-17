88ddd1f82d241fb1d0d5e8bcac1c4290bdbb7799
dudgns3443 <williams3443@gmail.com>
2021-12-17 11:04:11 +0900

    define outputs

--- terraform_configure/01_network/output.tf
+++ terraform_configure/01_network/output.tf
@@ -1,3 +1,21 @@
 output "a4_vpc_web_id" {
-  value = module.network.a4_id
+  value = module.network.a4_vpc_web_id
+}
+output "a4_vpc_was_id" {
+  value = module.network.a4_vpc_was_id
+}
+output "a4_sub_pub_web" {
+  value = module.network.a4_sub_pub_web
+}
+output "a4_sub_pri_web" {
+  value = module.network.a4_sub_pri_web
+}
+output "a4_sub_pub_was" {
+  value = module.network.a4_sub_pub_was
+}
+output "a4_sub_pri_was" {
+  value = module.network.a4_sub_pri_was
+}
+output "a4_sub_pri_db" {
+  value = module.network.a4_sub_pri_db
 }
--- terraform_template/network/output.tf
+++ terraform_template/network/output.tf
@@ -1,3 +1,21 @@
-output "vpc1_id" {
+output "a4_vpc_web_id" {
   value = aws_vpc.a4_vpc_web.id
+}
+output "a4_vpc_was_id" {
+  value = aws_vpc.a4_vpc_was.id
+}
+output "a4_sub_pub_web" {
+  value = aws_subnet.a4_pub
+}
+output "a4_sub_pri_web" {
+  value = aws_subnet.a4_priweb
+}
+output "a4_sub_pub_was" {
+  value = aws_subnet.a4_pubwas
+}
+output "a4_sub_pri_was" {
+  value = aws_subnet.a4_priwas
+}
+output "a4_sub_pri_db" {
+  value = aws_subnet.a4_pridb
 }
