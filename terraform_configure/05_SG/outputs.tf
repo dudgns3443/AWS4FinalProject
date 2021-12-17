ffa2831dbc35ffee67b6510a1b677f8222e991ba
swlee <allmaster1145@gmail.com>
2021-12-17 11:03:46 +0900

    sg module

--- terraform_configure/05_SG/outputs.tf
+++ terraform_configure/05_SG/outputs.tf
@@ -1,27 +1,27 @@
 output "alb_sg_id" {
-    value = module.sg.outputs.alb_sg_id
+    value = module.sg.alb_sg_id
 }
 
 output "bastion_sg_id" {
-    value = module.sg.outputs.bastion_sg_id
+    value = module.sg.bastion_sg_id
 }
 
 output "db_sg" {
-    value = module.sg.outputs.db_sg_id
+    value = module.sg.db_sg_id
 }
 
 output "efs_sg" {
-    value = module.sg.outputs.efs_sg_id
+    value = module.sg.efs_sg_id
 }
 
 output "gi_sg" {
-    value = module.sg.outputs.gi_sg_id
+    value = module.sg.gi_sg_id
 }
 
 output "was_sg" {
-    value = module.sg.outputs.was_sg_id
+    value = module.sg.was_sg_id
 }
 
 output "web_sg" {
-    value = module.sg.outputs.web_sg_id
+    value = module.sg.web_sg_id
 }
