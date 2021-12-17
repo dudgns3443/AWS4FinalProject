86f8ba1cef48e37e9c55d0eb052f8f82cd835e52
dudgns3443 <williams3443@gmail.com>
2021-12-17 19:33:56 +0900

    was.sh

--- terraform_configure/04_golden_img/was.sh
+++ terraform_configure/04_golden_img/was.sh
@@ -9,8 +9,8 @@ yum install -y nodejs
 yum install git -y
 git clone https://github.com/dudgns3443/HealthCare-BackEnd.git /app
 
-
+cd /app
 npm install package.json --save
 
-node /app/HealthCare-BackEnd/app &
-# nohup node /app/HealthCare-BackEnd/app &
+node /app/app &
+#nohup node /app/HealthCare-BackEnd/app &
