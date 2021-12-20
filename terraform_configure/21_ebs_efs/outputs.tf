89a401324e41c63ba2cd2e30ae30d5894a223a88
swlee <allmaster1145@gmail.com>
2021-12-20 11:25:29 +0900

    efs az

--- terraform_configure/21_ebs_efs/outputs.tf
+++ terraform_configure/21_ebs_efs/outputs.tf
@@ -1,2 +1,3 @@
 output "efs_id" {
-    value = module.ebs_efs.a4_efs_id
+    value = module.efs_ebs.a4_efs_id
+}
--- terraform_template/efs_ebs/outputs.tf
+++ terraform_template/efs_ebs/outputs.tf
@@ -1,3 +1,3 @@
-output "a4_Sefs_id" {
+output "a4_efs_id" {
    value = aws_efs_file_system.a4_efs.id
 }
