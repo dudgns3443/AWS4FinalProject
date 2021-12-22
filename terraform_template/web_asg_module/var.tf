4794fc94be232c9858faa3c2dd61aac23167a028
jinokay <kimjoe1997@naver.com>
2021-12-22 17:27:38 +0900

    var4

--- terraform_template/web_asg_module/var.tf
+++ terraform_template/web_asg_module/var.tf
@@ -1,12 +1,4 @@
-# variable "access_key" {
-#     type = string
-#     default = ""
-# }
 
-# variable "secret_key" {
-#     type = string
-#     default = ""
-# }
 variable "web_min_auto" {
     type = number
     default = 2
