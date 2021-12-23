4806554b669c377801901a090cd1d4bca1134902
jinokay <kimjoe1997@naver.com>
2021-12-23 09:10:57 +0900

    var2

--- terraform_template/alb_module/99_var.tf
+++ terraform_template/alb_module/99_var.tf
@@ -47,3 +47,12 @@ variable "unhealthy_threshold" {
     type = number
     default = 2
 }
+variable "b_protocol_http" {
+    type = string
+    default = "HTTP"
+}
+
+variable "lis_default_action" {
+    type = string
+    default = "forward"
+}
