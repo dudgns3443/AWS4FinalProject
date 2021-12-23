686878cad117cd2e5ecec1ac770c7a64bb0a44a2
dudgns3443 <williams3443@gmail.com>
2021-12-23 15:38:04 +0900

    필요없는 폴더 삭제

--- terraform_template/10_redis/redis.tf
+++ terraform_template/10_redis/redis.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "cluster-example"
   engine               = "redis"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.medium"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis6.x"
   engine_version       = "6.x"
